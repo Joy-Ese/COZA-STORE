42a1b3b1bd7bf878432d221328ee2f2ca805b8a3
Joy-Ese <86070579+Joy-Ese@users.noreply.github.com>
2022-07-24 14:49:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php include_once("html/coza-store.html"); ?>
+<?php include_once("html/shop.html"); ?>
+<?php include_once("html/blog.html"); ?>
+<?php include_once("html/blogdetails.html"); ?>
+<?php include_once("html/features.html"); ?>
+<?php include_once("html/latest.Styles.html"); ?>
+<?php include_once("html/contact.html"); ?>
